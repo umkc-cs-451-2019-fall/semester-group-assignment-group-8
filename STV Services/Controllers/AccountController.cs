2519743280d1ba63f8975289202766c7d6ffe461
Balino, Justin L.(UMKC-Student) <jlb3n5@mail.umkc.edu>
2019-11-02 12:01:18 -0500

    Add project files.

---
+++ STV Services/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using STV_Services.Models;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+public class AccountController : Controller
+{
+    // GET: Account, Login Screen
+    [HttpGet]
+    public ActionResult Login()
+    {
+        return View();
+    }
+    [HttpPost]
+
+    // verify username and password
+    public ActionResult Verify(Account acc)
+    {
+        List<Account> account = new List<Account>();
+        string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString; //setting variable to connection string
+        using (MySqlConnection con = new MySqlConnection(constr))
+        {
+            string query = "select * from users where username = '" + acc.Name + "' and usrpassword ='" + acc.Password + "'"; //select username and password from users table in schema
+            using (MySqlCommand cmd = new MySqlCommand(query))
+            {
+                cmd.Connection = con; //start running commands thru program
+                con.Open();
+                using (MySqlDataReader sdr = cmd.ExecuteReader())
+                {
+                    if (sdr.Read()) //if input is within table, login succesful 
+                    {
+                        con.Close();
+                        return View("Create");
+                    }
+                    else
+                    {
+                        con.Close(); //if input not in table, login not successful
+                        return View("Error");
+
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ STV Services/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace STV_Services.Models
+{
+    public class Account //Name = username, Password = password respectively
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
